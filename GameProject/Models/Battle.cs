b179b564945b2eba28af424dba3857d71b8a6020
Kex6awn <fferdinard1@gmail.com>
2024-12-10 20:26:32 -0600

    battle model

---
+++ GameProject/Models/Battle.cs
@@ -0,0 +1,15 @@
+﻿namespace GameProject.Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public int Team1Id { get; set; }
+        public Team Team1 { get; set; }
+        public int Team2Id { get; set; }
+        public Team Team2 { get; set; }
+        public int? WinnerTeamId { get; set; } // Null until the battle is decided
+        public Team WinnerTeam { get; set; }
+        public DateTime BattleDate { get; set; } = DateTime.Now;
+
+    }
+}
