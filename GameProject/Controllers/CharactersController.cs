203a5094c4e29e79576b99d128f5bbb3ef948a0c
Kex6awn <fferdinard1@gmail.com>
2024-12-10 20:17:59 -0600

    Character Model Validation

--- GameProject/Controllers/CharactersController.cs
+++ GameProject/Controllers/CharactersController.cs
@@ -1,6 +1,65 @@
-﻿namespace GameProject.Controllers
+﻿using GameProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GameProject.Controllers
 {
-    public class CharactersController
+    public class CharactersController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public CharactersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Characters/Customize
+        public IActionResult Customize(int? id)
+        {
+            Character character;
+
+            if (id.HasValue)
+            {
+                // Edit existing character
+                character = _context.Characters.FirstOrDefault(c => c.Id == id);
+                if (character == null)
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                // Create new character
+                character = new Character();
+            }
+
+            return View(character);
+        }
+
+        // POST: Characters/Customize
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Customize(Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                if (character.Id == 0)
+                {
+                    // New character
+                    _context.Characters.Add(character);
+                }
+                else
+                {
+                    // Update existing character
+                    _context.Characters.Update(character);
+                }
+
+                _context.SaveChanges();
+                return RedirectToAction("Details", "Team", new { id = character.TeamId });
+            }
+
+            return View(character);
+        }
     }
+
 }
--- GameProject/Models/Character.cs
+++ GameProject/Models/Character.cs
@@ -1,10 +1,19 @@
-﻿namespace GameProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameProject.Models
 {
     public class Character
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
         public string Name { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Health must be between 1 and 100.")]
         public int Health { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Strength must be between 1 and 100.")]
         public int Strength { get; set; }
 
         // Foreign key for Team
