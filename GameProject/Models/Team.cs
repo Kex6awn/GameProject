14166982b9f5d0087f831ba027f551fbad14ddf1
Kex6awn <fferdinard1@gmail.com>
2024-12-10 19:58:05 -0600

    Add project files.

---
+++ GameProject/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace GameProject.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Relationship with characters
+        public List<Character> Characters { get; set; } = new();
+    }
+
+}
